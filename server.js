d03788f6b28f76c47cfb4b9bd3f226f39ee039e5
mrain00 <luxassilva@gmail.com>
2022-04-19 12:03:35 -0300

    okey

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const Router = require('./routes/routes')
 const passport = require('passport')
 const app = express()
-
+const path = require('path')
 //middlewares
 app.use(cors())
 app.use(express.json())
@@ -13,15 +13,15 @@ app.use('/api', Router)
 
 
 //si la aplicacion se encuentra en produccion 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     //la carpeta de archivos estaticos va a ser la ruta siguiente
     app.use(express.static('client/build'))
     //cualquier pedido de tipo get
-    app.get('*', (req, res)=>{
+    app.get('*', (req, res) => {
         //va a devolver el archivo index.html
         //porque es la unica vista ya que react se encarga de renderizar todas las "paginas" del sitio
         res.sendFile(path.join(__dirname + 'client/build/index.html'))
     })
 }
 
-app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', ()=>console.log(`server ready on port ${process.env.PORT || 4000}`))
+app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => console.log(`server ready on port ${process.env.PORT || 4000}`))
